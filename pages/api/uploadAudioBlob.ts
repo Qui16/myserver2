378be42ed15d684932ca7cd1fcec41bde0f302c2
Qui16 <quydam1609@gmail.com>
2024-03-15 19:36:36 +1100

    Add @types/formidable dependency and update uploadAudioBlob.ts

--- pages/api/uploadAudioBlob.ts
+++ pages/api/uploadAudioBlob.ts
@@ -3,28 +3,49 @@ import multer from 'multer';
 import stream from 'stream';
 import uploadBlob from '../../uploadBlob';
 
+// Extend the NextApiRequest type to include the file property
+interface MulterRequest extends NextApiRequest {
+    file: Express.Multer.File;
+}
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+// Initialize multer
+const upload = multer();
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+export default async (req: MulterRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        const chunks = [];
-        for await (const chunk of req) {
-            chunks.push(chunk);
-        }
 
-        const buffer = Buffer.concat(chunks);
+        const multerAny: any = multer();
+        multerAny.single('file')(req, res, async (err: any) => {
+            if (err) {
+                return res.status(500).send(err.message);
+            }
 
-        try {
+            // The file is now in req.file
+            const buffer: Buffer = req.file.buffer;
+
+            // Create a readable stream from the Buffer
             const readableStream = new stream.PassThrough();
             readableStream.end(buffer);
-            await uploadBlob(readableStream);
-            res.status(200).send('File uploaded to Azure Blob Storage.');
-        } catch (err: any) {
-            console.error(`Error: ${err.message}`);
-            res.status(500).send(err.message);
-        }
+
+            try {
+                console.log('read', readableStream);
+                await uploadBlob(readableStream);
+                res.status(200).send('File uploaded to Azure Blob Storage.');
+            } catch (err: any) {
+                console.error(`Error: ${err.message}`);
+                res.status(500).send(err.message);
+            }
+        });
+
     } else {
         // Handle any other HTTP method
         res.setHeader('Allow', ['POST']);
